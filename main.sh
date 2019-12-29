11a9eda8b2abbe600723037f5d096c42b479949e
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2019-12-29 22:15:09 +0000

    learning the build cycle & updating main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,26 @@
-# echo Hello World
-echo off
+clear 
+echo '+------------------+'
+cal
+echo '+------------------+'
+date +'%d/%m/%Y %H:%M:%S'
+echo '+------------------+'
+echo
+
+# kotlinc hello.kt -include-runtime -d hello.jar
+# including any other javac flags you like
+# java -jar hello.jar
+# ./path-to-sample/sample
+
+# intro=$PWD/"01 Preliminaries/01 Introduction"
+
+echo "building: "
+echo '+------------------+'
+echo "hello.kt"
+echo '+------------------+'
+echo "binaries in ./Results"
+echo '+------------------+'
 
 kotlinc hello.kt -include-runtime -d hello.jar
-java -jar hello.jar
 
+# gcc "$intro/"hello.c -o ./Results/hello
+#./Results/gcd
