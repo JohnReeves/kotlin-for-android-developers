b1a402d2b9681608e0e4c380033f09a6609a5069
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2020-01-21 13:29:11 +0000

    fibonacci

--- hello.kt
+++ hello.kt
@@ -1,14 +1,26 @@
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+//import java.time.LocalDateTime
+//import java.time.format.DateTimeFormatter
 
 
 fun main(args: Array<String>) {
     println("Hello, World!")
+
+//    val current = LocalDateTime.now()
+//    val formatter = DateTimeFormatter.BASIC_ISO_DATE
+//    val formatted = current.format(formatter)
+//    println("Current Date is: $formatted")
+
+    val n = 10
+    var t1 = 0
+    var t2 = 1
     
-    val current = LocalDateTime.now()
-    val formatter = DateTimeFormatter.BASIC_ISO_DATE
-    val formatted = current.format(formatter)
-    println("Current Date is: $formatted")
+    print("First $n terms: ")
+    for (i in 1..n) {
+        print("$t1 + ")
+        val sum = t1 + t2
+        t1 = t2
+        t2 = sum
+    }
 
   val cyphertext="WKH HDVLHVW PHWKRG RI HQFLSKHULQJ D WHAW PHVVDJH LV WR UHSODFH HDFK FKDUDFWHU EB DQRWKHU XVLQJ D ILAHG UXOH, VR IRU HADPSOH HYHUB OHWWHU D PDB EH UHSODFHG EB G, DQG HYHUB OHWWHU E EB WKH OHWWHU H DQG VR RQ.";
 
